a9564daa6198de12ec456986a62a139f984380e3
Ethan S <es5888@nyu.edu>
2023-10-07 21:27:07 -0400

    Added navigation panel

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
         // Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-        // Add a tile layer (you can use different map providers)
+        var map = L.map('map').setView([51.505, -0.09], 13);
+    
+        // Add a tile layer
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-
+    
         // Add a marker to the map
         var marker = L.marker([51.5, -0.09]).addTo(map);
-
+    
         // Add a popup to the marker
         marker.bindPopup("<b>Hello, this is a Leaflet map!</b>").openPopup();
+    });
+    
