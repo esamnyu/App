f88d3a317dda71f4156ecd75f3e85dfcd9747554
Ethan S <es5888@nyu.edu>
2023-10-08 00:32:08 -0400

    Added changes to the side-panel branch to be looked at at 10/8

---
+++ scripts/experienceSystem.js
@@ -0,0 +1,51 @@
+class User {
+    constructor() {
+        this.xp = 0;
+        this.level = 1;
+    }
+
+    addXP(amount) {
+        this.xp += amount;
+        this.checkLevelUp();
+    }
+
+    checkLevelUp() {
+        let nextLevelThreshold = this.getThresholdForNextLevel();
+        while (this.xp >= nextLevelThreshold) { 
+            this.level++;
+            this.xp -= nextLevelThreshold; // subtract the threshold of the just surpassed level
+            nextLevelThreshold = this.getThresholdForNextLevel(); // get the threshold for the new level for the next iteration (if needed)
+            alert('You leveled up to Level ' + this.level);
+        }
+    }
+    
+
+    getThresholdForNextLevel() {
+        return this.level * 100;
+    }
+}
+
+const user = new User();
+
+document.getElementById("earnXPBtn").addEventListener("click", function() {
+    user.addXP(20); 
+    updateXpDisplay();
+});
+
+function updateXpDisplay() {
+    document.getElementById("userXP").innerText = user.xp;
+    document.getElementById("userLevel").innerText = user.level;
+    const xpToNextLevel = user.getThresholdForNextLevel();
+    document.getElementById("xpToNextLevel").innerText = xpToNextLevel; // Update the max XP for the next level
+    const fillPercentage = (user.xp / xpToNextLevel) * 100;
+    document.getElementById("xpFill").style.width = `${fillPercentage}%`;
+}
+
+// Initialization function to sync everything when the page loads
+function initializeExperienceSystem() {
+    updateXpDisplay();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    initializeExperienceSystem();
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,85 @@
 document.addEventListener("DOMContentLoaded", function() {
-        // Initialize the map
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        // Map Initialization
+        initializeMap();
     
-        // Add a tile layer
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        // Set up the chat drag functionality
+        setupChatDrag();
+    
+        // Initially, no chat user is selected
+        let currentChatUser = null;
+        const messagingPanel = document.querySelector(".messaging-panel");
+        document.getElementById("openMessagingBtn").addEventListener("click", function() {
+           //const messagingPanel = document.querySelector(".messaging-panel");
+                messagingPanel.classList.toggle("hidden");
+        });
+        // Other event listeners and DOMContentLoaded related code can go here
+    });
+
+    // Initialize the Leaflet map
+function initializeMap() {
+        const map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://s.tile.openstreetmap.org/z/x/y.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
     
-        // Add a marker to the map
-        var marker = L.marker([51.5, -0.09]).addTo(map);
-    
-        // Add a popup to the marker
+        const marker = L.marker([51.5, -0.09]).addTo(map);
         marker.bindPopup("<b>Hello, this is a Leaflet map!</b>").openPopup();
-    });
+    }
+    
+    // Function to open a chat with a user
+    function openChat(user) {
+        currentChatUser = user;
+        document.getElementById("chatHeader").innerText = `Chatting with ${user}`;
+        document.getElementById("messages").innerText = "";  // Clear chat when switching users
+    
+        const msgBtn = document.querySelector(".message-button");
+        msgBtn.addEventListener("click", function() {
+            sendMessage();
+        });
+    }
+    
+    // Function to send a message
+    function sendMessage() {
+        if (!currentChatUser) {
+            alert("Please select a contact to send a message.");
+            return;
+        }
+        const messageInput = document.getElementById("messageInput");
+        const messagesDiv = document.getElementById("messages");
+        if (messageInput.value.trim()) {  // Check if the message isn't just whitespace
+            messagesDiv.innerHTML += `<div>You: ${messageInput.value}</div>`;
+            messageInput.value = "";
+        }
+    }
+
+    // Set up the chat panel to be draggable
+function setupChatDrag() {
+        let isDragging = false;
+        let offsetX, offsetY;
+        const chatPanel = document.querySelector(".messaging-panel");
+    
+        chatPanel.addEventListener("mousedown", function(event) {
+            isDragging = true;
+            offsetX = event.clientX - this.getBoundingClientRect().left;
+            offsetY = event.clientY - this.getBoundingClientRect().top;
+        });
+    
+        document.addEventListener("mouseup", function() {
+            isDragging = false;
+        });
+    
+        document.addEventListener("mousemove", function(event) {
+            if (isDragging) {
+                const x = event.clientX - offsetX;
+                const y = event.clientY - offsetY;
+                chatPanel.style.left = x + "px";
+                chatPanel.style.top = y + "px";
+                chatPanel.style.position = "absolute";
+            }
+        });
+    }
+    
+    // Call the setupChatDrag function once to initialize the dragging functionality
+    setupChatDrag();
     
