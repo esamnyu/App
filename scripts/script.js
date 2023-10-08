2bda7114bcba702b2927ed333325f55b4512e3b2
Ethan S <es5888@nyu.edu>
2023-10-08 03:57:12 -0400

    Navgational Pane, and Chat Box now appear, implemented the logic for the socket - continue tomorrow 10/8

--- scripts/script.js
+++ scripts/script.js
@@ -89,3 +89,15 @@ function initializeMap() {
         sidePanel.classList.toggle("closed");
     });
     
+    $(function () {
+        var socket = io();
+        $('form').submit(function() {
+          socket.emit('chat message', $('#m').val());
+          $('#m').val('');
+          return false;
+        });
+        socket.on('chat message', function(msg){
+          $('#messages').append($('<li>').text(msg));
+        });
+      });
+      
